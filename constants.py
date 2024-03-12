7ac87f72c2114205b3f231bdbe51d54a0e78a40e
Hasan Iqbal <swapniliqbal@gmail.com>
2024-03-12 09:08:52 -0400

    refactor: go back to one everything in one file

--- constants.py
+++
@@ -1,9 +0,0 @@
-# Define initial global constants for the ROI
-# INSTRUCTIONS: run the script, adjust the trackbars to define the region of interest, and then use the printed values to replace the constants
-
-HORIZON = 63
-BOTTOM_TRIM = 92
-LEFT_MARGIN = 21
-RIGHT_MARGIN = 75
-TOP_LEFT_MARGIN = 48
-TOP_RIGHT_MARGIN = 57
